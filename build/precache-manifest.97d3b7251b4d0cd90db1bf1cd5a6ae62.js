a6a1f3590e107f751b76e95cbdd40b3967c73aa2
Danny Adams <dantheado@gmail.com>
2020-06-12 16:50:44 +0100

    Build folder for site deployment

---
+++ build/precache-manifest.97d3b7251b4d0cd90db1bf1cd5a6ae62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000dd8069adcd5acd692fbe2e4157d6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74d6f5c3b7d212dee577",
+    "url": "/static/css/main.ddabd54f.chunk.css"
+  },
+  {
+    "revision": "8f4cf7437386db875c75",
+    "url": "/static/js/2.7d0af485.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7d0af485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d6f5c3b7d212dee577",
+    "url": "/static/js/main.c1f58cef.chunk.js"
+  },
+  {
+    "revision": "e51e81ce461be16b84d6",
+    "url": "/static/js/runtime-main.8ba9af62.js"
+  }
+]);
